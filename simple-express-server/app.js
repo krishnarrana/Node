2e844e3fed329be8cd600c5235bbb8192d9b69b6
Krishna <krishna2121.rana@gmail.com>
2019-11-03 22:26:31 +0100

    nodemailer addded

--- simple-express-server/app.js
+++ simple-express-server/app.js
@@ -6,11 +6,45 @@ const port = 3000;
 var nodemailer = require('nodemailer');
 
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-    res.send('Hello world')
+    res.render('index')
+})
+app.get('/index', function (req, res) {
+    res.render('index')
+})
+app.get('/about', function (req, res) {
+    res.render('about')
+})
+app.get('/contact', function (req, res) {
+    res.render('contact')
+})
+app.post('/contact/send', function (req, res) {
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: '',
+            pass: ''
+        }
+    });
+    var mailOptions = {
+        from: 'Krishna Rana',
+        to: '',
+        text: 'Hello',
+        html: '<h1>Hello</h1>'
+    }
+    transporter.sendMail(mailOptions, function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message Sent:')
+        }
+    })
 })
 
 app.listen(port, () => {
